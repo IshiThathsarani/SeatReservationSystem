66e903b2da5851b53356dca926273144701e2b7c
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2024-08-11 16:17:23 +0530

    update the page

--- server/routes/setRouters.js
+++ server/routes/setRouters.js
@@ -4,8 +4,14 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+// import express from 'express';
+// import userController from '../controllers/userController.js';
+// import { Router } from 'express';
+
+
+
 router.get('/', userController.getSeats);
 router.post('/reserve', userController.reserveSeat);
 router.post('/cancel', userController.cancelReservation);
 
-module.exports = router;
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import connectDB from './config/db.js';
+import seatRouter from './routes/seatRouter.js';
+
 const port = 4000;
 
+dotenv.config();
 const app = express();
 connectDB();
+
+app.use(express.json());
+app.use('/api/seats', seatRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
