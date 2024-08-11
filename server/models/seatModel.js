476f6a82947d2f986c87818c93cbdf70d6a410e2
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2024-08-11 12:48:15 +0530

    create the seat model

---
+++ server/models/seatModel.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const seatSchema = new Schema({
+    seatNumber: { type: Number, required: true, unique: true },
+    reserved: { type: Boolean, required: true, default: false },
+    reservedBy: { type: String },
+}, {
+    timestamps: true,
+});
+
+const Seat = mongoose.model('Seat', seatSchema);
+
+export default Seat;
