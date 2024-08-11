5b9660355bc0384ce8190bf93e16c02afcae933f
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2024-08-11 12:38:52 +0530

    link the db page to server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import connectDB from './config/db.js';
 const port = 4000;
 
 const app = express();
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
